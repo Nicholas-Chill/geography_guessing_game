4b8738caced2848daecb6d3726ba4020381b918f
Nicholas Chill <11b3827@gmail.com>
2023-12-13 13:54:35 +0100

    added back, next, give up, and restart buttons

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const trophyImage = document.getElementById("win");
 const checkMark = document.getElementById("check");
 const startButton = document.getElementById("start");
 const input = document.getElementById("guess");
-const submit = document.getElementById("submit")
+const submit = document.getElementById("submit");
+const next = document.getElementById("next");
+const previous = document.getElementById("last");
+const flagImage = document.getElementById("flag");
+const endGame = document.getElementById("end");
+const restart = document.getElementById("restart");
+const timer = document.getElementById("timer");
 
 let score = 0;
 
@@ -16,17 +22,23 @@ let minutes2 = 0;
 
 let clock;
 
-let flagsShown = []
+let flagOrder = [];
 
-getRandomFlag();
+next.disabled = true;
+previous.disabled = true;
+endGame.disabled = true;
+restart.disabled = true;
+trophyImage.style.display = "none";
 
 function startGame() {
     startButton.disabled = true;
-    //setInterval(checkGuess, 100);
-    clock = setInterval(timer, 1000);
+    clock = setInterval(countUpTimer, 1000);
     input.disabled = false;
     input.focus();
     submit.disabled = false;
+    next.disabled = false;
+    previous.disabled = false;
+    endGame.disabled = false;
 }
 
 function showScore() {
@@ -34,29 +46,30 @@ function showScore() {
 }
 
 function getRandomFlag() {
-    if (flagsShown.length != flagArray.length) {
+    if (flagOrder.length != flagArray.length) {
         let randomIndex = Math.floor(Math.random() * flagArray.length);
-        while (flagsShown.includes(randomIndex)) {
-            randomIndex = Math.floor(Math.random() * flagArray.length);
-        }
-
         let selectedFlag = flagArray[randomIndex];
-        document.getElementById("flag").src = `./flags/${selectedFlag}`;
-        flagsShown.push(randomIndex);
-    } else {
-        clearInterval(clock);
-        trophyImage.style.display = "inline-flex";
+        while (flagOrder.includes(selectedFlag)) {
+            randomIndex = Math.floor(Math.random() * flagArray.length);
+            selectedFlag = flagArray[randomIndex];
+        }
+        return randomIndex;
     }
 }
 
 export function checkGuess() {
     const guess = input.value.toLowerCase().concat('.png');
-    if (guess == flagArray[flagsShown[flagsShown.length - 1]]) {
+    if (guess == flagOrder[currentFlagIndex]) {
         submit.click();
+        flagOrder.splice(currentFlagIndex, 1);
         checkMark.style.display = "inline-flex";
         increaseScore();
         setTimeout(hideCheckMark, 750);
-        getRandomFlag();
+        if (currentFlagIndex == flagOrder.length) {
+            currentFlagIndex = 0;
+        }
+        showFlag(flagOrder[currentFlagIndex]);
+        console.log(currentFlagIndex);
         flagForm.reset();
     }
 }
@@ -70,30 +83,110 @@ function hideCheckMark() {
     checkMark.style.display = "none";
 }
 
-function timer() {
-    if(minutes2 == 9 && seconds1 == 5 && seconds2 == 9) {
+function countUpTimer() {
+    if (minutes2 == 9 && seconds1 == 5 && seconds2 == 9) {
         minutes1++;
         minutes2 = 0;
         seconds1 = 0;
         seconds2 = 0;
-    } else if(seconds1 == 5 && seconds2 == 9) {
+    } else if (seconds1 == 5 && seconds2 == 9) {
         minutes2++;
         seconds1 = 0;
         seconds2 = 0;
-    } else if(seconds2 == 9) {
+    } else if (seconds2 == 9) {
         seconds1++;
         seconds2 = 0;
     } else {
         seconds2++;
     }
 
-    document.getElementById("timer").innerHTML = `Time: ${minutes1}${minutes2}:${seconds1}${seconds2}`;
+    timer.innerHTML = `Time: ${minutes1}${minutes2}:${seconds1}${seconds2}`;
+}
+
+function getNextFlag() {
+    let nextFlagIndex = currentFlagIndex + 1;
+
+    if (nextFlagIndex > flagOrder.length - 1) {
+        nextFlagIndex = 0;
+    }
+
+    currentFlagIndex = nextFlagIndex;
+  
+    showFlag(flagOrder[nextFlagIndex]);
+    input.focus();
+    flagForm.reset();
+}
+
+function getPreviousFlag() {
+    let lastFlagIndex = currentFlagIndex - 1;
+
+    if (lastFlagIndex < 0) {
+        lastFlagIndex = flagOrder.length - 1;
+    }
+
+    currentFlagIndex = lastFlagIndex;
+
+    showFlag(flagOrder[lastFlagIndex])
+    input.focus();
+    flagForm.reset();
+}
+
+function showFlag(flag) {
+    if (flagOrder.length == 0) {
+        clearInterval(clock);
+        trophyImage.style.display = "inline-flex";
+        previous.disabled = true;
+        next.disabled = true;
+        input.disabled = true;
+    } else {
+        flagImage.src = `./flags/${flag}`;
+    }
+}
+
+function giveUp() {
+    clearInterval(clock);
+    previous.disabled = true;
+    next.disabled = true;
+    input.disabled = true;
+    restart.disabled = false;
+}
+
+function createFlagOrder() {
+    for (let i = 0; i < flagArray.length; i++) {
+        let randomIndex = getRandomFlag();
+        flagOrder.push(flagArray[randomIndex]);
+    }
+}
+
+function restartGame() {
+    score = 0;
+    seconds1 = 0;
+    seconds2 = 0;
+    minutes1 = 0;
+    minutes2 = 0;
+    timer.innerHTML = `Time: ${minutes1}${minutes2}:${seconds1}${seconds2}`;
+    showScore();
+    flagOrder = [];
+    createFlagOrder();
+    showFlag(flagOrder[0]);
+    restart.disabled = true;
+    startButton.disabled = false;
+    endGame.disabled = true;
 }
 
 showScore();
 
 hideCheckMark();
-trophyImage.style.display = "none";
+
 
 startButton.addEventListener('click', startGame);
 flagForm.addEventListener('input', checkGuess);
+next.addEventListener('click', getNextFlag);
+previous.addEventListener('click', getPreviousFlag);
+endGame.addEventListener('click', giveUp);
+restart.addEventListener('click', restartGame);
+
+createFlagOrder();
+
+let currentFlagIndex = 0;
+showFlag(flagOrder[0]);
