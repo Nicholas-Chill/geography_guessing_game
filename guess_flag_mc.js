42244191d361ff47d0c8fae79f6560a1ffa29d73
Nicholas Chill <11b3827@gmail.com>
2024-02-28 00:55:20 +0100

    added almost everything needed

--- guess_flag_mc.js
+++ guess_flag_mc.js
@@ -8,23 +8,21 @@ const previous = document.getElementById("last");
 const flagImage = document.getElementById("flag");
 const endGame = document.getElementById("end");
 const restart = document.getElementById("restart");
-const timer = document.getElementById("timer");
 const choice1 = document.getElementById("choice1");
 const choice2 = document.getElementById("choice2");
 const choice3 = document.getElementById("choice3");
 const choice4 = document.getElementById("choice4");
+const choices = document.getElementsByClassName("choices");
 
 let score = 0;
-
-let seconds1 = 0;
-let seconds2 = 0;
-let minutes1 = 0;
-let minutes2 = 0;
-
-let clock;
-
 let flagOrder = [];
+let guess = "";
+var buttonClicked; 
 
+choice1.disabled = true;
+choice2.disabled = true;
+choice3.disabled = true;
+choice4.disabled = true;
 next.disabled = true;
 previous.disabled = true;
 endGame.disabled = true;
@@ -46,31 +44,20 @@ function showScore() {
     document.getElementById('score').innerHTML = `Score: ${score}/${flagArray.length}`
 }
 
-function getRandomFlag() {
-    if (flagOrder.length != flagArray.length) {
-        let randomIndex = Math.floor(Math.random() * flagArray.length);
-        let selectedFlag = flagArray[randomIndex];
-        while (flagOrder.includes(selectedFlag)) {
-            randomIndex = Math.floor(Math.random() * flagArray.length);
-            selectedFlag = flagArray[randomIndex];
-        }
-        return randomIndex;
-    }
+function getRandomFlagIndex() {
+    let randomIndex = Math.floor(Math.random() * flagArray.length);
+    randomIndex = Math.floor(Math.random() * flagArray.length);
+    return randomIndex;
 }
 
 export function checkGuess() {
-    const guess = input.value.toLowerCase().concat('.png');
-    if (guess == flagOrder[currentFlagIndex]) {
-        submit.click();
-        flagOrder.splice(currentFlagIndex, 1);
-        checkMark.style.display = "inline-flex";
+    if (guess.concat(".png") == flagOrder[currentFlagIndex]) {
+        buttonClicked.style.backgroundColor = 'green';
         increaseScore();
-        setTimeout(hideCheckMark, 750);
-        if (currentFlagIndex == flagOrder.length) {
-            currentFlagIndex = 0;
-        }
-        showFlag(flagOrder[currentFlagIndex]);
-        flagForm.reset();
+        setTimeout(changeChoicesColorToWhite, 750);
+        setTimeout(getNextFlag, 750);
+    } else {
+        buttonClicked.style.backgroundColor = 'red';
     }
 }
 
@@ -91,10 +78,8 @@ function getNextFlag() {
     }
 
     currentFlagIndex = nextFlagIndex;
-  
     showFlag(flagOrder[nextFlagIndex]);
-    input.focus();
-    flagForm.reset();
+    randomChoiceOptions();
 }
 
 function getPreviousFlag() {
@@ -106,14 +91,12 @@ function getPreviousFlag() {
 
     currentFlagIndex = lastFlagIndex;
 
-    showFlag(flagOrder[lastFlagIndex])
-    input.focus();
-    flagForm.reset();
+    showFlag(flagOrder[lastFlagIndex]);
+    randomChoiceOptions();
 }
 
 function showFlag(flag) {
     if (flagOrder.length == 0) {
-        clearInterval(clock);
         trophyImage.style.display = "inline-flex";
         previous.disabled = true;
         next.disabled = true;
@@ -126,32 +109,35 @@ function showFlag(flag) {
 }
 
 function giveUp() {
-    clearInterval(clock);
     previous.disabled = true;
     next.disabled = true;
-    input.disabled = true;
     restart.disabled = false;
     endGame.disabled = true;
+    choice1.disabled = true;
+    choice2.disabled = true;
+    choice3.disabled = true;
+    choice4.disabled = true;
 }
 
 function createFlagOrder() {
     for (let i = 0; i < flagArray.length; i++) {
-        let randomIndex = getRandomFlag();
+        let randomIndex = getRandomFlagIndex();
+        while (flagOrder.includes(flagArray[randomIndex])) {
+            randomIndex = getRandomFlagIndex();
+        }
         flagOrder.push(flagArray[randomIndex]);
     }
 }
 
 function restartGame() {
+    changeChoicesColorToWhite();
+    currentFlagIndex = 0;
     score = 0;
-    seconds1 = 0;
-    seconds2 = 0;
-    minutes1 = 0;
-    minutes2 = 0;
-    timer.innerHTML = `Time: ${minutes1}${minutes2}:${seconds1}${seconds2}`;
     showScore();
     flagOrder = [];
     createFlagOrder();
     showFlag(flagOrder[0]);
+    randomChoiceOptions();
     restart.disabled = true;
     startButton.disabled = false;
     endGame.disabled = true;
@@ -161,32 +147,26 @@ function restartGame() {
 function randomChoiceOptions() {
     let choices = [choice1, choice2, choice3, choice4];
     const randomChoiceAnswer = choices[Math.floor(Math.random() * choices.length)];
-    choices.splice(randomChoiceAnswer, 1);
-    
-    let randomChoiceOrder = [];
-    for (let i = 0; choices.length; i++) {
-        let randomIndex = Math.floor(Math.random() * choices.length);
-        if (randomChoiceOrder.includes(randomIndex)) {
-            while (randomChoiceOrder.includes(randomIndex)) {
-                randomIndex = Math.floor(Math.random() * choices.length);
-            }
-        }
-        randomChoiceOrder.push(randomIndex);
-    }
-
-    let indexes = [];
-    for (let i = 0; i < randomChoiceOrder.length; i++) {
-        let randomIndex = Math.floor(Math.random() * flagArray.length);
-        if (indexes.includes(randomIndex)) {
-            while (indexes.includes(randomIndex)) {
-                randomIndex = Math.floor(Math.random() * flagArray.length);
-            }
-        }
-        indexes.push(randomIndex);
-        randomChoiceOrder[i].innerHTML = flagArray[indexes[0]].replace(".png", "");
-    }
-
+    choices.splice(choices.indexOf(randomChoiceAnswer), 1);
     randomChoiceAnswer.innerHTML = flagOrder[currentFlagIndex].replace(".png", "");
+
+    let flagOptions = [];
+    for (let i = 0; i < choices.length; i++) {
+        let flagIndex = getRandomFlagIndex();
+        let flag = flagArray[flagIndex];
+        while (flagOptions.includes(flag) || flag == flagOrder[currentFlagIndex]) {
+            flagIndex = getRandomFlagIndex();
+            flag = flagArray[flagIndex];
+        }
+        flagOptions.push(flag);
+        choices[i].innerHTML = flagOptions[i].replace(".png", "");
+    }
+}
+
+function changeChoicesColorToWhite() {
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].style.backgroundColor = "white";
+    }
 }
 
 showScore();
@@ -194,10 +174,36 @@ showScore();
 hideCheckMark();
 
 startButton.addEventListener('click', startGame);
-next.addEventListener('click', getNextFlag);
-previous.addEventListener('click', getPreviousFlag);
+next.addEventListener('click', () => {
+    changeChoicesColorToWhite();
+    getNextFlag();
+});
+previous.addEventListener('click', () => {
+    changeChoicesColorToWhite();
+    getPreviousFlag();
+});
 endGame.addEventListener('click', giveUp);
 restart.addEventListener('click', restartGame);
+choice1.addEventListener('click', (e) => {
+    guess = e.target.innerHTML;
+    buttonClicked = choice1;
+    checkGuess();
+});
+choice2.addEventListener('click', (e) => {
+    guess = e.target.innerHTML;
+    buttonClicked = choice2;
+    checkGuess();
+});
+choice3.addEventListener('click', (e) => {
+    guess = e.target.innerHTML;
+    buttonClicked = choice3;
+    checkGuess();
+});
+choice4.addEventListener('click', (e) => {
+    guess = e.target.innerHTML;
+    buttonClicked = choice4;
+    checkGuess();
+});
 
 createFlagOrder();
 
@@ -206,3 +212,5 @@ let currentFlagIndex = 0;
 showFlag(flagOrder[0]);
 
 randomChoiceOptions();
+
+console.log(choices);
